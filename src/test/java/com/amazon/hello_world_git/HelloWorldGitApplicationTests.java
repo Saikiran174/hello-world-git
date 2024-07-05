d275041460d4c807e985d8306697cb236b7404cf
saikiran <sainani655@gmail.com>
2024-07-05 00:43:26 -0500

    new files added

---
+++ src/test/java/com/amazon/hello_world_git/HelloWorldGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazon.hello_world_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
